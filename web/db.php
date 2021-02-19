400aa80ec68458189a86a53b37fd8437073c84fb
JacobEduardo <ojoescher@gmail.com>
2021-02-18 22:59:30 -0300

    coneccion a base de datos y separacion de header y footer

---
+++ web/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'loan_system'
+);
+
+if (isset($conn)) {
+    echo 'DB is conection';
+}
+
+?>
--- web/index.php
+++ web/index.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+<?php include("includes/header.php") ?>
+
 <div>
 <form method="post" action="" name="signup-form">
 <h1>Ingrese sus credenciales</h1>
@@ -21,5 +16,4 @@
 </form>
 </div>
 
-</body>
-</html>
+<?php include("includes/footer.php")  ?>
