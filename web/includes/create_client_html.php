ed529ad7818f82b33d2a67f174bd499b3ccce3a6
jparraguezv <jparraguezv@SNO_ARL01>
2021-02-23 18:54:19 -0300

    remplazondo con funciones

---
+++ web/includes/create_client_html.php
@@ -0,0 +1,50 @@
+<?php
+    while ($rows = $query_result->fetch_array()){
+        $rut = $rows['RUT'];
+        $name = $rows['NAME'];
+        $status = $rows['STATUS'];
+    }
+?>
+
+<table>
+  <tr>
+    <th><?php echo $name ?></th>
+    <th><?php echo "" ?></th>
+    <th><?php echo "" ?></th>
+  </tr>
+  <tr>
+    <td><?php echo $rut ?></td>
+    <td><?php echo "" ?></td>
+    <td><?php echo "" ?></td>
+</tr>
+<tr>
+    <td colspan="3" style="text-align:right;">
+    <?php if($status == 1){ ?>
+    <FORM METHOD="POST" ACTION="">
+        <input  name="goods" placeholder="Buscar" class="form-control form-text" type="text" size="20" maxlength="150" />
+        <input  name="loan" type="submit" value="Prestar"/>
+    <?php } 
+        require("db.php");    
+
+        if(isset($_POST['loan'])){
+            $seeker = $_POST['goods'];
+    
+            $query = "SELECT * FROM goods WHERE code LIKE '%$goods%';";
+            $query_result = mysqli_query($conn,$query);  
+            $result_rows = mysqli_num_rows($query_result);
+    
+            if($result_rows > 0){
+                if($result_rows == 1){
+                    require("includes/create_goods_html.php");
+                }
+                if($result_rows > 1){
+                    
+                }
+            }
+        }
+    ?>
+    </FORM>
+    </td>
+</tr>
+</table>
+
--- web/websites/inicio.php
+++ web/websites/inicio.php
@@ -4,29 +4,35 @@
     <input id="seeker_btn" name="search" type="submit" value="Buscar"/>
 
     <?php
-    require("db.php");    
+    require("db.php");  
+    require("consultas.php");  
 
     if(isset($_POST['search'])){
         $seeker = $_POST['seeker'];
 
-        $query = "SELECT * FROM goods WHERE code LIKE '%$seeker%';";
-        $query_result = mysqli_query($conn,$query);  
+        
+        $query_result = search_in_goods($seeker,$conn);  
         $result_rows = mysqli_num_rows($query_result);
+
         if($result_rows > 0){
             if($result_rows == 1){
-                while ($rows = $query_result->fetch_array()){
-                    $name = $rows['NAME'];
-                    $value = $rows['VALUE'];
-                    $loan_status = $rows['LOAN_STATUS'];
-                    $description = $rows['DESCRIPTION'];
-                    $code = $rows['CODE'];
-                    $invertory_status = $rows['INVENTORY_STATUS'];
-                    $id_location = $rows['ID_LOCATION'];
-                }
-                echo '<div>' .$name .'</div>';
+                require("includes/create_goods_html.php");
             }
             if($result_rows > 1){
-                echo 'asd';
+                
+            }
+        }
+
+        $query = "SELECT * FROM client WHERE rut LIKE '%$seeker%';";
+        $query_result = mysqli_query($conn,$query);  
+        $result_rows = mysqli_num_rows($query_result);
+
+        if($result_rows > 0){
+            if($result_rows == 1){
+                require("includes/create_client_html.php");
+            }
+            if($result_rows > 1){
+                
             }
         }
     }
